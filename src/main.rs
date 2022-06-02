ea291c992ceb8ec5f9ab422cee7d57524cadc8e5
crisleo94 <cristhian.reinoso@live.com>
2022-06-01 20:15:08 -0500

    create function to recieve input, create function to start the game, change the function to get the json file to get all the available decks, create function to recieve the players name

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,14 @@ use std::fs::File;
 use std::error::Error;
 
 #[derive(Deserialize, Serialize, Debug)]
-struct WhiteCard {
+pub struct WhiteCard {
     #[serde(default)]
     text: String,
     pack: u8,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
-struct BlackCard {
+pub struct BlackCard {
     #[serde(default)]
     text: String,
     pack: u8,
@@ -20,7 +20,7 @@ struct BlackCard {
 }
 
 #[derive(Deserialize, Serialize, Debug)]
-struct CardPack {
+pub struct Deck {
     #[serde(default)]
     name: String,
     official: bool,
@@ -28,30 +28,53 @@ struct CardPack {
     black: Vec<BlackCard>
 }
 
-fn main() {
-    read_json().unwrap();
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Player {
+    #[serde(default)]
+    name: String,
+    //cards: Vec<WhiteCard>
 }
 
-pub fn read_json() -> Result<(), Box<dyn Error>> {
-    let players = vec!["Player 1", "Player 2", "Player 3", "Player 4"]; 
-    let mut black_cards_text: Vec<String> = Vec::new();
-    let mut white_cards_text: Vec<String> = Vec::new();
+// main function
+fn main() {
 
-    let file = File::open("src/assets/json/full.json")?;
+}
 
-    let pack: Vec<CardPack> = serde_json::from_reader(file)?;
-    
+// get a list of all the decks, each deck contains a different set of cards
+pub fn get_decks() -> Result<Vec<Deck>, Box<dyn Error>> {
+    let mut decks: Vec<Deck> = Vec::new();
+    let file = File::open("src/assets/json/full.json").unwrap();
+    let pack: Vec<Deck> = serde_json::from_reader(file).unwrap();
+
     for val in pack {
-        for card in val.black {
-            black_cards_text.push(card.text);
-        }
-        for card in val.white {
-            white_cards_text.push(card.text);
-        }
+        decks.push(val);
     }
 
-    println!("{}", black_cards_text.len());
-    println!("{}", white_cards_text.len());
-    
-    Ok(())
+    Ok(decks)
+}
+
+// game start
+
+pub fn start_game() {
+    let get_decks_result: Result<Vec<Deck>, Box<dyn Error>> = get_decks();
+    let decks: Vec<Deck> = get_decks_result.unwrap();
+
+    println!("Please enter the players name separated with a coma: ");
+    let players_input = get_input();
+    let players: Vec<&str> = players_input.split(',').collect();
+
+}
+
+pub fn get_input() -> String {
+    let mut input = String::new();
+
+    match std::io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            return input.trim().split_whitespace().collect();
+        },
+        Err(error) => {
+            println!("Error: {}", error);
+            String::new()
+        }
+    }
 }
